f246b929adeb0ee2ba673b5da637002bf056749b
Tom Atkins <tom@knitatoms.net>
2014-03-05 13:37:02 +0000

    Add os.environ settings

--- memuri/config/settings.py
+++ memuri/config/settings.py
@@ -389,6 +389,11 @@ class Production(Common):
     ########## END TEMPLATE CONFIGURATION
 
     ########## CACHING
+
+    os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', '')
+    os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
+    os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
+
     # Only do this here because thanks to django-pylibmc-sasl and pylibmc memcacheify is painful to install on windows.
     CACHES = {
       'default': {
